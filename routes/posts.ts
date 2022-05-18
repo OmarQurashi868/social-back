d3857c78b78f543a6b1717587724d5e3f4d442fa
Omar Kamal <OmarQurashi868@gmail.com>
2022-05-18 23:52:06 +0400

    Added new route for infinte scroll

--- routes/posts.ts
+++ routes/posts.ts
@@ -2,12 +2,47 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 import Post from "../models/post";
 
+interface PostType {
+  _id: string;
+  title: string;
+  content: string;
+  creationDate: Date;
+  creatorId: string;
+}
+
+const compare = (a: PostType, b: PostType): number => {
+  if (a.creationDate >= b.creationDate) {
+    return -1;
+  } else {
+    return 1;
+  }
+};
+
+
 router.get("/allposts", async (req: Request, res: Response) => {
   try {
     const posts = await Post.find();
     if (posts.length < 1)
       return res.status(400).json({ message: "No posts found" });
+    posts.sort(compare);
     return res.status(200).json(posts);
+  } catch (err: any) {
+    return res.status(500).json({ message: err });
+  }
+});
+
+router.get("/limit/:start/:count", async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.find();
+    if (posts.length < 1)
+      return res.status(404).json({ message: "No posts found" });
+    const end = Number(req.params.start) + Number(req.params.count);
+    let limitedPosts = [];
+    for (let i = Number(req.params.start); i < end; i++) {
+      limitedPosts.push(posts[i]);
+    }
+    limitedPosts.sort(compare);
+    return res.status(200).json(limitedPosts);
   } catch (err: any) {
     return res.status(500).json({ message: err });
   }
